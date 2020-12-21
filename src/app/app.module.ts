91c269c22af3b59cb616ec6adae9e0983bb788a7
Victor <victorcuru@gmail.com>
2020-12-21 11:56:41 -0300

    Prototipando a pagina e estrutura inicial da home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { ButtonComponent } from './components/button/button.component';
+import { LoginComponent } from './pages/login/login.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    ButtonComponent
+    ButtonComponent,
+    LoginComponent,
+    UserCardComponent,
+    TabsComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/user-card/user-card.component.spec.ts
+++ src/app/components/user-card/user-card.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { UserCardComponent } from './user-card.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('UserCardComponent', () => {
+  let component: UserCardComponent;
+  let fixture: ComponentFixture<UserCardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ UserCardComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(UserCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
